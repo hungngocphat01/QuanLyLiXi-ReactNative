b404804e882f5f8067454481afaf17b5ae2af380
hungngocphat01 <hungngocphat01@gmail.com>
2022-01-28 14:51:13 +0700

    Change indentation

--- state-manager/recordSlice.js
+++ state-manager/recordSlice.js
@@ -2,73 +2,73 @@ import { createSlice } from "@reduxjs/toolkit";
 import Toast from 'react-native-toast-message';
 
 export const recordSlice = createSlice({
-	name: "records",
-	initialState: [],
-	reducers: {
-		addNewRecord: addRecordCallback,
-		deleteRecord: deleteRecordCallback
-	}
+  name: "records",
+  initialState: [],
+  reducers: {
+    addNewRecord: addRecordCallback,
+    deleteRecord: deleteRecordCallback
+  }
 });
 
 function addRecordCallback(state, action) {
-	// Every record is unique by CATEGORY and NAME
-	const { name, money, note, category } = action.payload;
-	if (name.length == 0 || money.length == 0 || category.length == 0) {
-		console.log("Empty! Refusing");
-		Toast.show({
-			type: "error",
-			text1: "Thông tin trống! Vui lòng kiểm tra lại.",
-			position: "bottom"
-		});
-		return;
-	}
+  // Every record is unique by CATEGORY and NAME
+  const { name, money, note, category } = action.payload;
+  if (name.length == 0 || money.length == 0 || category.length == 0) {
+    console.log("Empty! Refusing");
+    Toast.show({
+      type: "error",
+      text1: "Thông tin trống! Vui lòng kiểm tra lại.",
+      position: "bottom"
+    });
+    return;
+  }
 
-	const idx = state.findIndex(elem => (elem["name"] == name) && (elem["category"] == category));
+  const idx = state.findIndex(elem => (elem["name"] == name) && (elem["category"] == category));
 
-	if (idx == -1) {
-		const newRecord = {
-			name: name,
-			money: Number.parseInt(money),
-			note: note,
-			category: category,
-			date: (new Date()).toISOString()
-		};
-		state.push(newRecord);
-		console.log(newRecord);
+  if (idx == -1) {
+    const newRecord = {
+      name: name,
+      money: Number.parseInt(money),
+      note: note,
+      category: category,
+      date: (new Date()).toISOString()
+    };
+    state.push(newRecord);
+    console.log(newRecord);
 
-		Toast.show({
-			type: "success",
-			text1: `Đã thêm "${name}" (${category}) với ${money}đ`,
-			position: "bottom"
-		});
-	} else {
-		console.log("Object existed.");
+    Toast.show({
+      type: "success",
+      text1: `Đã thêm "${name}" (${category}) với ${money}đ`,
+      position: "bottom"
+    });
+  } else {
+    console.log("Object existed.");
 
-		Toast.show({
-			type: "info",
-			text1: `"${name}" (${category}) đã tồn tại`,
-			position: "bottom"
-		});
-	}
+    Toast.show({
+      type: "info",
+      text1: `"${name}" (${category}) đã tồn tại`,
+      position: "bottom"
+    });
+  }
 }
 
 function deleteRecordCallback(state, action) {
-	// Every record is unique by CATEGORY and NAME
-	const { name, money, note, category } = action.payload;
-	const idx = state.findIndex(elem => (elem["name"] == name) && (elem["category"] == category));
+  // Every record is unique by CATEGORY and NAME
+  const { name, money, note, category } = action.payload;
+  const idx = state.findIndex(elem => (elem["name"] == name) && (elem["category"] == category));
 
-	if (idx > -1) {
-		state.splice(idx, 1);
-		console.log("Item deleted.");
-		Toast.show({
-			type: "success",
-			text1: "Đã xóa thành công!",
-			text2: `${name} (${category})`,
-			position: "bottom"
-		})
-	} else {
-		console.log("Record not found. Cannot delete.");
-	}
+  if (idx > -1) {
+    state.splice(idx, 1);
+    console.log("Item deleted.");
+    Toast.show({
+      type: "success",
+      text1: "Đã xóa thành công!",
+      text2: `${name} (${category})`,
+      position: "bottom"
+    })
+  } else {
+    console.log("Record not found. Cannot delete.");
+  }
 }
 
 export const { addNewRecord, deleteRecord } = recordSlice.actions;
